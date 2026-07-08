826fc2f734919a8f5eb94597134188feafb4e739
nazar12345m <nazarsenyk19@gmail.com>
2026-07-08 14:52:21 +0300

    Fix formatting and spacing in main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=reply_markup
     )
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
